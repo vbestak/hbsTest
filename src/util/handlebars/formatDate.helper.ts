12737372a38218bb8c7ccd21f0a82725534fa6ef
Valentino Beštak <valentino.bestak@nobula.eu>
2024-06-08 11:52:53 +0200

    fix date not formated correctly in article create view

---
+++ src/util/handlebars/formatDate.helper.ts
@@ -0,0 +1,22 @@
+export function formatDateHelper(date: Date, format: string): string {
+  const pad = (number: number) => (number < 10 ? '0' : '') + number;
+
+  const replacements = {
+    yyyy: date.getFullYear(),
+    yy: String(date.getFullYear()).slice(-2),
+    MM: pad(date.getMonth() + 1),
+    M: date.getMonth() + 1,
+    dd: pad(date.getDate()),
+    d: date.getDate(),
+    hh: pad(date.getHours()),
+    h: date.getHours(),
+    mm: pad(date.getMinutes()),
+    m: date.getMinutes(),
+    ss: pad(date.getSeconds()),
+    s: date.getSeconds(),
+    SSS: String(date.getMilliseconds()).padStart(3, '0'),
+    S: date.getMilliseconds(),
+  };
+
+  return format.replace(/yyyy|yy|MM|M|dd|d|hh|h|mm|m|ss|s|SSS|S/g, match => replacements[match]);
+}
